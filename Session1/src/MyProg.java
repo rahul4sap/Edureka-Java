62c24f4b89a4b1c52c268a48d3df590720accf73
Rahul <rahul4sap@gmail.com>
2018-09-22 16:46:18 +0530

    Initial commit

---
+++ Session1/src/MyProg.java
@@ -0,0 +1,19 @@
+
+public class MyProg {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World !!");
+
+	}
+	
+}
+
+
+class One{
+	
+}
+
+class Two{
+	
+}
---
+++ Session2/src/DataTypes.java
@@ -0,0 +1,11 @@
+// In Java we write our programs in classes
+
+public class DataTypes {
+	
+	//main is a method (is a way or a procedure) where instructions written in it, gets executed sequentially
+	public static void main(String[] args) {
+		
+
+	}
+
+}
